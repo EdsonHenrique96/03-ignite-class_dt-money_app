aa07001abc187ae4e924ec08f02c0feb51785beb
Edson Batista <edson.batista@brmalls.com.br>
2024-01-15 23:00:12 -0300

    feat(Transaction Page): transaction table component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,40 @@
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
+import {
+  PriceHighlight,
+  TransactionContainer,
+  TransactionsTable,
+} from './styles';
 
 export function TransactionsPage() {
   return (
     <div>
       <Header />
       <Summary />
+      <TransactionContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Vendas</td>
+              <td>13/04/2023</td>
+            </tr>
+            <tr>
+              <td width="50%">Sushi</td>
+              <td>
+                <PriceHighlight variant="outcome">
+                  - R$ 10.000,00
+                </PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionContainer>
     </div>
   );
 }
